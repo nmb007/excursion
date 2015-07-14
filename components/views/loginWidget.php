2cbf583ac447d23a73b852f094dc57341cba6eb9
Rehan Ali <rehan.ali@metadesign.eu>
2015-07-14 09:17:58 +0500

    Replacing <?= to <?php ?>

--- components/views/loginWidget.php
+++ components/views/loginWidget.php
@@ -5,20 +5,20 @@ use yii\helpers\Html;
 <div class="panel panel-default">
   <div class="panel-body">
     <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-        <?= $form->field($user, 'username', [
+        <?php echo $form->field($user, 'username', [
               'inputOptions' => [
                   'placeholder' => $user->getAttributeLabel('username'),
               ],
             ])->label(false);
         ?>
-        <?= $form->field($user, 'password', [
+        <?php echo $form->field($user, 'password', [
               'inputOptions' => [
                   'placeholder' => $user->getAttributeLabel('password'),
               ],
             ])->passwordInput()->label(false);
         ?>
         <div class="form-group">
-            <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+            <?php echo Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
         </div>
     <?php ActiveForm::end(); ?>
   </div>
